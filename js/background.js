a7f8027e98857edf1ebdbe69daf2bf45dd43c970
Subrata Das <subratappt@gmail.com>
2023-12-09 21:10:55 -0500

    upgrade

--- js/background.js
+++ js/background.js
@@ -4,4 +4,14 @@ var ready = (callback) => {
 }
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
+
+    // For changing navbar color on scroll
+    window.onscroll = function () {
+        const navbar = document.querySelector('.navbar-fixed-top');
+        if (window.scrollY > 100) {
+            navbar.classList.add('scrolling');
+        } else {
+            navbar.classList.remove('scrolling');
+        }
+    };
 })
