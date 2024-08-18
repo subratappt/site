7e1e691b8b7576a95e9b6fe20fd7c89d8984e971
Subrata Das <subratappt@gmail.com>
2024-08-18 18:30:40 -0400

    major update

--- js/pubs.js
+++ js/pubs.js
@@ -2,57 +2,42 @@ var myArray = []
 $.getJSON("data/pub.json", function (jsonData) {
     // console.log(jsonData); 
     myArray = jsonData["items"];
-    console.log(myArray)
+    // console.log(myArray)
     buildTable(myArray)
 });
 
-// $('th').on('click', function () {
-//     var column = $(this).data('column')
-//     var order = $(this).data('order')
-//     var text = $(this).html()
-//     // console.log(text)
-//     // text = text.substring(0, text.length - 1)
 
-//     if (order == 'desc') {
-//         $(this).data('order', "asc")
-//         myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
-//         // text += '&#9660'
+function toggleAbstract(abstractId) {
+    var abstractDiv = document.getElementById(abstractId);
+    var abstractTrigger = abstractDiv.previousElementSibling; // Assuming the <b> tag is directly before the <div>
 
-//     } else {
-//         $(this).data('order', "desc")
-//         myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
-//         // text += '&#9650'
-
-//     }
-//     $(this).html(text)
-//     buildTable(myArray)
-// })
-
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip()
-// })
+    if (abstractDiv.style.display === "none") {
+        abstractDiv.style.display = "block";
+        abstractTrigger.innerHTML = abstractTrigger.innerHTML.replace('▶', '▼');
+    } else {
+        abstractDiv.style.display = "none";
+        abstractTrigger.innerHTML = abstractTrigger.innerHTML.replace('▼', '▶');
+    }
+}
 
 function buildTable(data) {
     var table = document.getElementById('myTable')
     table.innerHTML = ''
+    // Assuming data is an array of objects and each object has a date property
+    data.sort(function (a, b) {
+        var dateA = new Date(a.date); // Convert date string to Date object
+        var dateB = new Date(b.date); // Convert date string to Date object
+        return dateB - dateA; // Sort in descending order
+    });
+    var lastYear = null;
     for (var i = 0; i < data.length; i++) {
-        // var appsLink = data[i].applicationLink
-        // var link = "&#x1F5CF"
-        // if (typeof appsLink == "undefined") {
-        //     appsLink = ""
-        //     // link = "&#x1F5CF"
-        //     link = "&#x2709"
-        // }
-        // var deadline = data[i].deadline
-        // if (typeof deadline == "undefined") {
-        //     deadline = ""
-        // }
-        // var msg = data[i].message
-        // if (typeof msg == "undefined") {
-        //     msg = ""
-        // }
         var date = new Date(data[i].date)
         var yy = date.getFullYear()
+        if (lastYear !== null && yy !== lastYear) {
+            // If the year changes, insert a horizontal line
+            table.innerHTML += '<tr><td colspan="100%" style="padding: 0; height: auto;"><hr style="margin: 0;"></td></tr>';
+        }
+        lastYear = yy;
         var mm = date.getMonth()
         var creators = data[i].creators
         var authorList = ''
@@ -63,12 +48,23 @@ function buildTable(data) {
             }
             authorList += and + creators[j].firstName + " " + creators[j].lastName + ", ";
         }
+        var journal = null;
+        if (data[i].itemType == "journalArticle") {
+            dataIssue = data[i].issue ? " (" + data[i].issue + ")" : ""
+            journal = data[i].journalAbbreviation + " " + data[i].volume + dataIssue + ", " + data[i].pages
+        } else if (data[i].itemType == "preprint") {
+            journal = "arXiv preprint " + data[i].archiveID
+        }
 
-        var row = `<tr>
-                            <td  class=""> ${yy}</td>
-							<td  class="text-wrap">${authorList} ${data[i].title}, <a href=${data[i].url}>${data[i].journalAbbreviation} ${data[i].volume} (${data[i].issue}), ${data[i].pages}</a></td>
-					  </tr>`
-        table.innerHTML += row
+        var row = `<tr style="font-family: Garamond, serif; font-size:18px;">
+        <td>${yy}</td>
+        <td class="text-wrap"> <i style="color: #565656;">${data[i].title}</i>, ${authorList}  <a href="${data[i].url}" style="text-decoration: none;">${journal}</a> <b style="color: #565656; cursor: pointer;" onclick="toggleAbstract('abstract${i}')">▶ Abstract</b>
+            <div id="abstract${i}" style="display: none; font-family: 'Times New Roman'; color: black; margin-top: 10px;border: 1px solid #ccc; border-radius: 8px; padding: 10px;background-color: #f9f9f9;">
+                ${data[i].abstractNote ? data[i].abstractNote : "No abstract available."}
+            </div>
+        </td>
+   </tr>`;
+        table.innerHTML += row;
     }
 }
 
