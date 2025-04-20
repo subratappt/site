cd8f2a74cb46717a25382a890dd1b33dc1d78006
Subrata Das <subratappt@gmail.com>
2025-04-20 13:50:49 -0400

    update

--- js/pubs.js
+++ js/pubs.js
@@ -42,6 +42,7 @@ function buildTable(data) {
         var creators = data[i].creators
         var authorList = ''
         and = ''
+        var mynote = data[i].mynote ? data[i].mynote : ''
         for (var j = 0; j < creators.length; j++) {
             if (j > 0 && j == creators.length - 1) {
                 and = " and "
@@ -60,8 +61,8 @@ function buildTable(data) {
 
         var row = `<tr style="font-family: Garamond, serif; font-size:18px;">
         <td>${yy}</td>
-        <td class="text-wrap"> <i style="color: #565656;">${data[i].title}</i>, ${authorList}  <a href="${data[i].url}" style="text-decoration: none;">${journal}</a> ${data[i].mynote} <b style="color: #565656; cursor: pointer;" onclick="toggleAbstract('abstract${i}')">▶ Abstract</b>
-            <div id="abstract${i}" style="display: none; font-family: 'Times New Roman'; color: black; margin-top: 10px;border: 1px solid #ccc; border-radius: 8px; padding: 10px;background-color: #f9f9f9;">
+        <td class="text-wrap"> <i style="color: #565656;">${data[i].title}</i>, ${authorList}  <a href="${data[i].url}" style="text-decoration: none; color: #2f82a6">${journal}</a> ${mynote} <b style="color: #565656; cursor: pointer;" onclick="toggleAbstract('abstract${i}')">▶ Abstract</b>
+            <div id="abstract${i}" style="display: none; font-family: 'Times New Roman'; font-size:16px ; color: black; margin-top: 10px;border: 1px solid #ccc; border-radius: 8px; padding: 10px;background-color:#e8e8e8;">
                 ${data[i].abstractNote ? data[i].abstractNote : "No abstract available."}
             </div>
         </td>
